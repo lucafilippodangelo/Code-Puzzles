7a9ee9b3d2018cb5fc12edfe1ecb6fda60ea059d
Luca D'Angelo <info@lucadangelo.it>
2018-05-26 15:18:46 +0100

    num of islands

---
+++ IslandCount/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace IslandCount
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*
+             LD EXERCISE "Count Number of Islands"
+                 Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. 
+                 An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
+             SOLUTION "use visited array to store all the visited cell and use queue to expand islands"
+             */
+
+            countIslands();
+            Console.ReadLine();
+
+         }//main
+
+        private static void countIslands() {
+
+            //LD test 3 islands
+            int[,] ldArray;
+            ldArray = Utilities.createArray4x5with3Islands(); 
+            Utilities.countNumOfIslands(ldArray);
+
+            //LD test 1 island
+            int[,] ldArray002;
+            ldArray002 = Utilities.createArray4x5with1Island();
+            Utilities.countNumOfIslands(ldArray002);
+        }
+
+    }//program
+}//namespace
+
---
+++ IslandCount/Utilities.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IslandCount
+{
+    public static class Utilities
+    {
+        static bool[,] visited;
+
+        public static int[,] createArray4x5with3Islands()
+        {
+            //LD create
+            int[,] anArray = new int[4, 5] {
+                { 1,1,0,0,0 },
+                { 1,1,0,0,0 },
+                { 0,0,1,0,0 },
+                { 0,0,0,1,1 } };
+
+            consolePrintBidimensionalArray(anArray, 5);
+            //LD return
+            return anArray;
+        }
+
+        public static int[,] createArray4x5with1Island()
+        {
+            //LD create
+            int[,] anArray = new int[4, 5] {
+                { 1,1,1,1,0 },
+                { 1,1,0,1,0 },
+                { 1,1,0,0,0 },
+                { 0,0,0,0,0 } };
+
+            consolePrintBidimensionalArray(anArray, 5);
+            //LD return
+            return anArray;
+        }
+        
+        private static void consolePrintBidimensionalArray(int [,] anArray, int colNumber)
+        {
+            //LD console print
+            int rowSplit = 0;
+            foreach (int i in anArray)
+            {
+                if (rowSplit % colNumber == 0) { Console.WriteLine(); }
+                System.Console.Write("{0} ", i);
+                rowSplit++;
+            }
+        }
+
+        public static int countNumOfIslands(int[,] ldArray)
+        {
+            int count = 0;
+
+            var rows = ldArray.GetLength(0);
+            var columns = ldArray.GetLength(1);
+
+            visited = new bool[rows, columns];
+
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < columns; c++)
+                {
+                    if (ldArray[r, c] == 1 && !visited[r, c]) //if specific position is 1 and not visited yet
+                    {
+                        //LD discover recursevely and mark as visited all the closer "1" 
+                        NumIslandsHelper(ldArray, r, c);
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine(); Console.WriteLine(); Console.WriteLine("Number of Islands: " + count);
+
+            return count;
+        }//countAndPrintNumOfIslands
+
+        private static void NumIslandsHelper(int[,] ldArray, int r, int c)
+        {
+            //LD return if:
+            if (r < 0 || r >= ldArray.GetLength(0)) return;// "r" index bigger or smaller than grid row dimension
+            if (c < 0 || c >= ldArray.GetLength(1)) return;// "c" index bigger or smaller than grid column dimension
+            if (visited[r, c]) return;// the node is already visited
+            if (ldArray[r, c] != 1) return;// the node is zero
+
+            visited[r, c] = true;
+
+            //LD visit recursively:
+            NumIslandsHelper(ldArray, r - 1, c); // above
+            NumIslandsHelper(ldArray, r + 1, c); // below
+            NumIslandsHelper(ldArray, r, c - 1); // left
+            NumIslandsHelper(ldArray, r, c + 1); // right
+        }
+    }
+}
