7c5b33996168f7ad670cad04726680c6834a1437
Luca D'Angelo <info@lucadangelo.it>
2018-05-28 08:17:24 +0100

    -

--- ArrayLeftRotation/Program.cs
+++ ArrayLeftRotation/Program.cs
@@ -6,10 +6,20 @@ namespace ArrayLeftRotation
     {
         static void Main(string[] args)
         {
+            //LD test approach one
             int[] ldArray = { 1, 2, 3, 4, 5 };
             arrayLeftRotation(ldArray, 2);
+            
+
+            //LD test approach two
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+            leftRotate(arr, 2, 7);
+            printArray(arr, 7);
+
             Console.ReadLine();
         }
+
+        #region Approach ONE- rotate one to one
 
         private static void arrayLeftRotation(int[] anArray, int d)
         {
@@ -53,5 +63,64 @@ namespace ArrayLeftRotation
                 System.Console.Write("{0} ", i);
             }
         }
+
+        #endregion
+
+        #region Approach TWO(more performant) - divide the array in different sets before to rotate
+
+        /*
+          Instead of moving one by one, divide the array in different sets
+          where number of sets is equal to GCD of n and d and move the elements within sets.
+          If GCD is 1 as is for the above example array (n = 7 and d =2), then elements will be 
+          moved within one set only, we just start with temp = arr[0] and keep moving arr[I+d] 
+          to arr[I] and finally store temp at the right place.
+
+          EXAMPLE - n =12 and d = 3. GCD is 3 and:
+          - 1 2 3 4 5 6 7 8 9 10 11 12 (input)
+          - 4 2 3 7 5 6 10 8 9 1 11 12 (first step)
+          - 4 5 3 7 8 6 10 11 9 1 2 12 (second step)
+          - 4 5 6 7 8 9 10 11 12 1 2 3 (thirth step)
+
+         */
+
+        private static void printArray(int[] arr, int size)
+        {
+            for (int i = 0; i < size; i++)
+                Console.Write(arr[i] + " ");
+        }
+
+        //LD determine how big has to be each sub array
+        private static int gcd(int a, int b)
+        {
+            if (b == 0)
+                return a;
+            else
+                return gcd(b, a % b);
+        }
+
+        static void leftRotate(int[] arr, int d, int n) //array, num. of rotations, array size
+        {
+            int i, j, k, temp;
+            for (i = 0; i < gcd(d, n); i++)
+            {
+                /* move i-th values of blocks */
+                temp = arr[i];
+                j = i;
+                while (true)
+                {
+                    k = j + d;
+                    if (k >= n)
+                        k = k - n;
+                    if (k == i)
+                        break;
+                    arr[j] = arr[k];
+                    j = k;
+                }
+                arr[j] = temp;
+            }
+        }
+
+        #endregion
+
     }
 }
